7384b7c22f1e3a45c9994749b97d3954a4283db5
Oleg <o.yanchinskiy@gmail.com>
2016-06-20 23:08:12 -0700

    maxStockPrice

---
+++ stockPrices.js
@@ -0,0 +1,19 @@
+var stockPrices = [45, 44.4, 44.2, 44.8, 45, 45.1, 44.7, 44.9, 45.9, 45]
+var diminishStockPrices = [45, 44, 43, 42, 41, 40]
+
+var maxStockProfit = function (stockPrices) {
+  var minPrice = stockPrices[0]
+  var maxProfit = stockPrices[1] - stockPrices[0]
+
+  for (var i = 1; i < stockPrices.length; i++) {
+    var currentStockPrice = stockPrices[i]
+    var potentialProfit = currentStockPrice - minPrice
+
+    maxProfit = Math.max(maxProfit, potentialProfit)
+    minPrice = Math.min(minPrice, currentStockPrice)
+  }
+  return maxProfit
+}
+
+console.log(maxStockProfit(stockPrices))
+console.log(maxStockProfit(diminishStockPrices))
