45590c12fa9c8236af95ffad987c2ee2504e0f68
Oleg <o.yanchinskiy@gmail.com>
2016-06-29 22:07:52 -0700

    love rectange

---
+++ loveRectangle.js
@@ -0,0 +1,21 @@
+/*
+Write a function to find the rectangular intersection of two given love rectangles.
+
+As with the example above, love rectangles are always "straight" and never "diagonal."
+More rigorously: each side is parallel with either the x-axis or the y-axis.
+
+var myRectangle = {
+
+    // coordinates of bottom-left corner
+    leftX: 1,
+    bottomY: 5,
+
+    // width and height
+    width: 10,
+    height: 4,
+
+}
+*/
+
+function loveRectangle (rectangleOne, rectangleDos) {
+}
