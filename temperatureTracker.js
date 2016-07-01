b33e5ad2b2b546d1b3f9988a5b8d6bc2cfbda640
Oleg <o.yanchinskiy@gmail.com>
2016-06-30 21:51:27 -0700

    temperature initial setup

---
+++ temperatureTracker.js
@@ -0,0 +1,18 @@
+/*
+Write a class TempTracker with these methods:
+
+insert()—records a new temperature
+getMax()—returns the highest temp we've seen so far
+getMin()—returns the lowest temp we've seen so far
+getMean()—returns the mean ↴ of all temps we've seen so far
+getMode()—returns the mode ↴ of all temps we've seen so far
+
+Optimize for space and time. Favor speeding up the getter functions (getMax(), getMin(), getMean(), and getMode()) over speeding up the insert() function.
+
+Temperatures will all be inserted as integers.
+We'll record our temperatures in Fahrenheit, so we can assume they'll all be in the range 0..1100..110.
+
+*/
+
+function TemperatureTracker () {
+}
