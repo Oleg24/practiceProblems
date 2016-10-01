d7c2752be306be302e5e81115bd63c8820c4d1e1
Oleg <o.yanchinskiy@gmail.com>
2016-10-01 14:38:57 -0700

    check binaryTree

---
+++ checkBinaryTree.js
@@ -0,0 +1,69 @@
+// write a function to check that binary tree is a valid binary search tree 
+
+function BinaryTreeNode(value) {
+	      this.value = value;
+	      this.left  = null;
+	      this.right = null;
+  }
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+	this.left = new BinaryTreeNode(value);
+	return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+	    this.right = new BinaryTreeNode(value);
+	    return this.right;
+};
+
+function checkBinaryTree(root){
+	var bstree = true;
+	var rootValue = root.value;
+
+	if (root.left.value > root.value) {
+		return bstree = false;
+	} else if (root.right.value < root.value) {
+		return bstree = false 
+	} else if (root.left){
+		return checkBinaryTree(root.left)
+	} else if (root.right){
+		return checkBinaryTree(root.right)
+	}
+	return bsTree; 
+}
+
+
+function bstChecker(treeRoot) {
+
+    // start at the root, with an arbitrarily low lower bound
+    // and an arbitrarily high upper bound
+    var nodeAndBoundsStack = [];
+    nodeAndBoundsStack.push({node: treeRoot, lowerBound: -Infinity, upperBound: Infinity});
+
+    // depth-first traversal
+    while (nodeAndBoundsStack.length) {
+        var nodeAndBounds = nodeAndBoundsStack.pop();
+        var node = nodeAndBounds.node,
+            lowerBound = nodeAndBounds.lowerBound,
+            upperBound = nodeAndBounds.upperBound;
+
+        // if this node is invalid, we return false right away
+        if (node.value < lowerBound || node.value > upperBound) {
+            return false;
+        }
+
+        if (node.left) {
+            // this node must be less than the current node
+            nodeAndBoundsStack.push({node: node.left, lowerBound: lowerBound, upperBound: node.value});
+
+        }
+        if (node.right) {
+            // this node must be greater than the current node
+            nodeAndBoundsStack.push({node: node.right, lowerBound: node.value, upperBound: upperBound});
+        }
+    }
+
+    // if none of the nodes were invalid, return true
+    // (at this point we have checked all nodes)
+    return true;
+}
